6199025b5d2f45a5967672657f9722b272e8cf08
代东 <daidong123654@gmail.com>
2014-03-25 19:39:11 +0800

    Create Dev branch

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -7,10 +7,13 @@ class test extends CI_Controller{
 	function __construct()
 	{
 		# code...
+		parent::__construct();		
 	}
 
 	function index(){
-		echo "This is a test";
+		echo "This is a test<br/>";
+		echo base_url();
+		echo site_url();
 	}
 }
 ?>
